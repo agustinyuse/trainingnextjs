edf5dba1f4f219517a0dce1d4fb2c8231c5daa9a
Agustín Yusé <agustin.yuse@assurant.com>
2023-08-22 15:00:56 -0300

    acento fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ function Home() {
     <div className="flex flex-col gap-2 justify-center w-full items-center">
       <div>
         <h1 className="tracking-tight inline font-semibold text-4xl lg:text-6xl">
-          Bienvenido a la pagina de&nbsp;
+          Bienvenido a la página de&nbsp;
         </h1>
         <h1 className="tracking-tight inline font-semibold from-[#FF705B] to-[#FFB457] text-4xl lg:text-6xl bg-clip-text text-transparent bg-gradient-to-b">
           Rick And Morty
